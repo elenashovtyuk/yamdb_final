29557708a66fedf937e5221680314d0e66b112b1
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-14 16:43:45 +0300

    add changes in settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '%@agbt9d77c*o3w920w%1%!hhcajvxb=+l0y=o1x43#*96v#c^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
